7476396c1159abc6aa67dc5211aedec8f758bbb3
Asif Iqbal <Asif.Iqbal.8739@gmail.com>
2017-03-16 19:01:46 +1100

    Add files via upload
    Matlab code for simulation section from "MSDL fmRI" paper.
    
    ompbox (http://www.cs.technion.ac.il/~ronrubin/software.html) required to run the code. Please download and add them into your matlab path.

---
+++ MSDL_Temp.m
@@ -0,0 +1,198 @@
+%% Mult-Subject Dictionary learning along with Common Dictionary for Temporal Case
+% [D0,Di,X0,Xi] = argmin 0.5||Yi - D0X0 - DiXi||_F^2 + eta*||Di*A_/i||F^2
+% In this script, we learn X0 -> Xi -> D0 -> Di
+
+function [Dict_0,Dict,X_0,X] = MSDL_Temp(St)
+%% Some Data Setup
+[n,N] = size(St.Yn);    Ns = N/St.nSub;
+Data = cell(1,St.nSub);
+for i = 1:St.nSub
+    Data{i} = St.Yn(:,(i-1)*Ns+1:i*Ns);
+end
+
+%% Dictionary Initialization
+X = cell(1,St.nSub);                                  % Sub Specific Sparse Codes
+X_0 = zeros(St.Size_D0,Ns);
+Dict = cell(1,St.nSub);
+for i = 1:St.nSub
+    if St.DiniType == 1
+        if i == 1
+            Dict_0 = normc(St.Yn(:,randperm(N,St.Size_D0)));
+        end
+        Dict{i} = normc(St.Yn(:,randperm(N,St.Size_Ds)));
+    else
+        if i == 1
+            Dict_0 = normc(randn(n,St.Size_D0));
+        end
+        Dict{i} = normc(randn(n,St.Size_Ds));
+    end
+    X{i} = zeros(St.Size_Ds,Ns);
+end
+
+%% Dict Learning
+for iter = 1:St.nIter
+    [X_0,X] = SparseCode(Data,Dict_0,Dict,X_0,X,St.Spar,St.AlgoType);
+    if St.verbose == 1; displayError(Data,Dict_0,Dict,X_0,X,iter);  end
+    
+    [Dict_0,Dict] = DictUpdate2(Data,Dict_0,Dict,X_0,X,St);
+    if St.verbose == 1; displayError(Data,Dict_0,Dict,X_0,X,iter);  fprintf('\n');  end
+    
+end
+% fprintf('............END..........\n');   
+end
+
+%% Sparsity Implementation for MSDL_Temporal Scene
+% Old Sparse codes are needed for this step
+function [X_0,X] = SparseCode(Data,Dict_0,Dict,X_0,X,Spar,AlgoType)    
+    M = size(Dict,2);
+    
+    if AlgoType == 1
+        X_0 = SparseCommon;
+        X   = SparseIndividual;
+    elseif AlgoType == 2
+        X   = SparseIndividual;
+        X_0 = SparseCommon;
+    end        
+        
+    
+    % Inner Functions
+    function X_0_ = SparseCommon
+        Et = 0;
+        for i = 1:M
+            Et = Et + (Data{i} - Dict{i}*X{i}); 
+        end
+        Et = Et./M;
+        X_0_ = omp(Dict_0,Et,Dict_0'*Dict_0,Spar(1));
+    end
+
+    function X_ = SparseIndividual
+        for i = 1:M
+            Ei = Data{i} - Dict_0*X_0;
+            X_{i} = omp(Dict{i},Ei,Dict{i}'*Dict{i},Spar(2));
+        end
+    end
+end
+
+%% Dictionary Update Step Gradient Descent
+function [Dict_0,Dict] = DictUpdate(Data,Dict_0,Dict,X_0,X,St)
+    M = St.nSub;
+    if St.AlgoType == 1
+        DictCommon;
+        DictIndividual;
+    elseif St.AlgoType == 2
+        DictIndividual;
+        DictCommon;
+    end
+
+    % Inner Functions
+    function DictCommon
+        W = 0;  A0 = [];
+        for i = 1:M    
+            A0 = [A0,Dict{i}];
+            W = W + (Data{i} - Dict{i} * X{i});
+        end
+        DD = Dict_0;    W = W./M;   
+        A00 = A0*A0';   rho = 1/norm(full(X_0)*full(X_0)');
+        for k = 1:St.Kmax
+            tt = rho*((DD*X_0-W)*X_0' + St.eta*(A00)*DD);
+            DD = normc(DD - tt);    %- eye(size(DD,1))
+            if norm(tt,'fro') < St.eps;         break;   end
+        end
+        Dict_0 = DD;
+    end
+
+    function DictIndividual
+        for i = 1:M                
+            Ai = Dict_0;
+            for h = 1:M   
+                if h == i; continue; end 
+                Ai = [Ai,Dict{h}];   
+            end;
+            Gi = Data{i} - Dict_0*X_0;
+            DD = Dict{i};   XX = X{i};
+            Aii = Ai*Ai';   rho = 1/norm(full(XX)*full(XX)');
+            for k = 1:St.Kmax
+                tt = rho*((DD*XX-Gi)*XX' + St.eta*(Aii)*DD);    % - eye(size(DD,1))
+                DD = normc(DD - tt);
+                if norm(tt,'fro') < St.eps;    break;   end
+            end
+            Dict{i} = (DD);
+        end        
+    end
+end
+
+%% Approximation Error to display
+function displayError(Data,Dict_0,Dict,X_0,X,iter)
+    for b = 1:size(Dict,2)
+       EE(b) = (norm(Data{b} - Dict_0*X_0 - Dict{b}*X{b},'fro')); 
+    end
+    display([sprintf('Iter:%2d, Total Error: ',iter),sprintf('%0.2f,\t',EE)]) %
+end
+
+%% Dictionary Update Step Using ALM
+function [Dict_0,Dict] = DictUpdate2(Data,Dict_0,Dict,X_0,X,St)
+    M = St.nSub;    max_mu = St.max_mu;     scale_mu = St.scale_mu;   ini_mu = St.ini_mu;
+    Z0 = zeros(size(Dict_0));   mu = ini_mu;    
+    Zi = zeros(size(Dict{1}));  W0 = Z0;    Wi = Zi; 
+    if St.AlgoType == 1
+        DictCommon;
+        DictIndividual;
+    elseif St.AlgoType == 2
+        DictIndividual;
+        DictCommon;
+    end
+
+    % Inner Functions
+    function DictCommon
+        T = 0;  A0 = [];
+        Z0 = zeros(size(Dict_0));   mu = ini_mu; W0 = Z0;
+        for i = 1:M    
+            A0 = [A0,Dict{i}];
+            T = T + (Data{i} - Dict{i} * X{i});
+        end
+        DD = Dict_0;    T = T./M;   
+        A00 = A0*A0';   %rho = 1/norm(full(X_0)*full(X_0)');
+        for k = 1:St.Kmax
+%             while norm((DD - Z0),'fro') > 0.01
+                DD = (mu*Z0 + T*X_0' - W0)/(X_0*X_0' + mu*eye(size(DD,2)));  DD = normc(DD);
+                Z0 = (mu*eye(size(DD,1))+ 2*St.eta*A00)\(W0 + mu*DD); Z0 = normc(Z0);
+%             end
+            W0 = W0 + mu*(DD-Z0);
+            mu = min(scale_mu*mu,max_mu);
+%             display(norm((DD - Z0),'fro'))
+            if norm((DD - Z0),'fro') < St.eps;  break;  end
+        end
+        Dict_0 = DD;
+    end
+
+    function DictIndividual
+        for i = 1:M        
+            Zi = zeros(size(Dict{i}));   mu = ini_mu; Wi = Zi;
+            Ai = Dict_0;
+            for h = 1:M   
+                if h == i; continue; end 
+                Ai = [Ai,Dict{h}];   
+            end;
+            Gi = Data{i} - Dict_0*X_0;
+            DD = Dict{i};   XX = X{i};
+            Aii = Ai*Ai';   % rho = 1/norm(full(XX)*full(XX)');
+            for k = 1:St.Kmax
+%                 while norm((DD - Zi),'fro') > 0.01
+                    DD = (mu*Zi + Gi*XX' - Wi)/(XX*XX' + mu*eye(size(DD,2)));  DD = normc(DD);
+                    Zi = (mu*eye(size(DD,1))+ 2*St.eta*Aii)\(Wi + mu*DD); Zi = normc(Zi);
+%                 end
+                Wi = Wi + mu*(DD-Zi);
+                mu = min(scale_mu*mu,max_mu);
+            
+                if norm((DD - Zi),'fro') < St.eps;    break;         end
+            end
+            Dict{i} = (DD);
+        end        
+    end
+end
+
+
+
+
+
+
---
+++ MainSimScript.m
@@ -0,0 +1,98 @@
+%% Script to call dictionary learning on the SIMTB dataset
+clear all; close all;
+% clc; 
+load('SimTB_Data');
+sigma = 0.2;   
+rng(65);
+
+%% Dictionary Learning Parameters Setup
+param.Size_D0 = 10;      % Common Dict Size
+param.Size_Ds = 10;      % Particular Dict Size
+param.Spar = [2,3];     % Dict Sparsities
+param.nSub = Sim.nSub;         % Number of Subjects
+param.eta = 0.5;        % Regularization penalty parameter
+param.rho = 0.001;      % Gradient Descent step size
+param.eps = 10^-4;      % parameter to end dict learning iterations earlier
+param.Kmax = 40;        % Max Grad Descent iterations
+param.nIter = 20;       % Number of iterations (Sparse coding -> Dict Learning)
+param.verbose = 0;      % Intermediate results display (Error n stuff)
+param.DiniType = 2;     % Dict Initialization: 1 -> From data, 2 -> Random
+param.AlgoType = 1;     % 1 -> Common -> Individual, 2 -> Individual -> Common
+% Parameters for ADMM
+param.max_mu = 10^10;
+param.scale_mu = 2.0;
+param.ini_mu = 10^-4;
+Trials = 2;
+
+% Count Variables
+nComp = param.nSub+3;   % Total Components TC/SMs
+[Count_TC,Count_SM,Count_TCI,Count_SMI] = deal(cell(nComp,1));        % Correlation Counts
+TC = Sim.TC;                     % Time Courses
+SM = [Sim.SMCommon;cell2mat(Sim.SMSpec')];  % Spatial Maps
+
+for tr = 1:Trials  
+    fprintf('Trial:%d\n',tr);
+    param.Yn = Sim.Data + sigma*randn(size(Sim.Data));          % Noisy Dataset
+%% Dict learning script call
+% tic
+    [Dict_0,Dict,X_0,X] = MSDL_Temp(param);
+% toc   
+    DD = Dict;  DD{param.nSub+1} = Dict_0;  % Forr correlation n stuff
+    XX = X;     XX{param.nSub+1} = X_0;
+
+
+
+%% Correlation stuff for Time Courses      
+    for k = 1:param.nSub+1
+        for c = 1:nComp
+        [Count_TC{c}(tr,k),Count_TCI{c}(tr,k)] =  max(abs(corr(TC(:,c),DD{k})));  
+        
+% Correlation stuff for Spatial Maps
+        [Count_SM{c}(tr,k),Count_SMI{c}(tr,k)] =  max(abs(corr(SM(c,:)',XX{k}')));
+        end
+    end
+
+end
+
+%% Displaying the Correlation Results
+for i = 1:nComp 
+    fprintf('Mean Correlation of TC_%02d:, %0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f\n',i,mean(Count_TC{i}));
+    MeanTC(i,:) = mean(Count_TC{i});
+end;    fprintf('\n');
+for i = 1:nComp 
+    fprintf('Mean Correlation of SM_%02d:, %0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f,%0.4f\n',i,mean(Count_SM{i}));
+    MeanSM(i,:) = mean(Count_SM{i});
+end
+
+%% Display the original info
+% Mask = Sim.Mask;
+% Disp_Act2(TC,SM,Mask);
+% 
+% %% Display the results
+% Disp_Act2(Dict_0,full(X_0),Mask);
+% for i = 1:param.nSub
+%     Disp_Act2(Dict{i},full(X{i}),Mask);
+% end
+
+%% Plots for COrrelations
+figure();
+imagesc(MeanTC);
+set(gca, 'XTick', 1:7); % center x-axis ticks on bins
+set(gca, 'YTick', 1:9); % center y-axis ticks on bins
+Xlabel = {'D_1','D_2','D_3','D_4','D_5','D_6','D_0'};
+Ylabel = {'TC_1','TC_2','TC_3','TC_4','TC_5','TC_6','TC_7','TC_8','TC_9'};
+set(gca, 'XTickLabel', Xlabel); % set x-axis labels
+set(gca, 'YTickLabel', Ylabel); % set y-axis labels
+colorbar;   %colormap hot;
+title('Mean TC correlations')
+
+figure();
+imagesc(MeanSM);
+set(gca, 'XTick', 1:7); % center x-axis ticks on bins
+set(gca, 'YTick', 1:9); % center y-axis ticks on bins
+Xlabel = {'X_1','X_2','X_3','X_4','X_5','X_6','X_0'};
+Ylabel = {'S_1','S_2','S_3','S_4','S_5','S_6','S_7','S_8','S_9'};
+set(gca, 'XTickLabel', Xlabel); % set x-axis labels
+set(gca, 'YTickLabel', Ylabel); % set y-axis labels
+colorbar;   %colormap hot;
+title('Mean SM correlations')
